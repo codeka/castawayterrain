cec80c78dc0314a80ed60aac38e537d605b62eea
Dean Harding <dean@codeka.com.au>
2019-06-14 23:00:30 -0700

    Make sure we only generate the overworld and not the nether or end.

--- src/main/java/com/codeka/castawayterrain/world/CastawayWorldType.java
+++ src/main/java/com/codeka/castawayterrain/world/CastawayWorldType.java
@@ -2,6 +2,7 @@ package com.codeka.castawayterrain.world;
 
 import net.minecraft.world.World;
 import net.minecraft.world.WorldType;
+import net.minecraft.world.dimension.OverworldDimension;
 import net.minecraft.world.gen.ChunkGenerator;
 
 public class CastawayWorldType extends WorldType {
@@ -11,6 +12,10 @@ public class CastawayWorldType extends WorldType {
 
     @Override
     public ChunkGenerator<?> createChunkGenerator(World world) {
-        return new CastawayChunkGenerator(world);
+        if (world.getDimension().isSurfaceWorld()) {
+            return new CastawayChunkGenerator(world);
+        }
+
+        return super.createChunkGenerator(world);
     }
 }
